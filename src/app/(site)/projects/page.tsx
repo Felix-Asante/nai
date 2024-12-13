da3ca144f15da40b4d0d60a469aaf7deb351b823
felix <felix@PROs-MacBook-Pro.local>
2024-12-13 17:54:20 +0100

    fetch all projects

--- src/app/(site)/projects/page.tsx
+++ src/app/(site)/projects/page.tsx
@@ -1,18 +1,27 @@
 import Container from "@/components/layouts/Container";
 import MainNavbar from "@/components/navbars/MainNavbar";
-import { PROJECTS } from "@/constants/content";
+import { getProjects } from "@/lib/sanity/api/projects";
 import ProjectHeader from "@/sections/projects/ProjectHeader";
 import ProjectsList from "@/sections/ProjectsList";
-import React from "react";
 
-export default function ProjectPage() {
+type Props = {
+  params: Promise<{
+    page: number;
+    category: string;
+  }>;
+};
+
+export default async function ProjectPage(props: Props) {
+  const { page = 1, category } = await props.params;
+
+  const data = await getProjects({ page: page - 1, limit: 12, category });
   return (
     <main>
       <MainNavbar className="text-neutral-300 bg-white" />
       <ProjectHeader />
 
       <Container className="my-10">
-        <ProjectsList projects={PROJECTS} />
+        <ProjectsList projects={data.items} />
       </Container>
     </main>
   );
